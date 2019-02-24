0ba54c35a259ba68b2be95dd319ee7b9456c9ca7
min <apollo96@naver.com>
2019-02-24 17:36:10 +0900

    Blog project2

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404,redirect
 from django.utils import timezone
 from django.core.paginator import Paginator
 from .models import Blog
+from .form import BlogPost 
 
 def home(request):
     blogs = Blog.objects
@@ -30,3 +31,17 @@ def create(request):
     blog.pub_date = timezone.datetime.now()
     blog.save()
     return redirect('/blog/' + str(blog.id))
+
+def blogpost(request):
+    #1.입력된 내용을 처리하는 기능-->POST
+    if request.method =='POST':
+        form = BlogPost(request.POST)
+        if form.is_valid():
+            post =form.save(commit=False)
+            post.pub_date = timezone.now()
+            post.save()
+            return redirect('home')
+    #2.빈 페이지를 띄워주는 기능-->GET
+    else:
+        form = BlogPost()
+        return render(request,'blog/new.html',{'form':form})    
